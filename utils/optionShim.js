2a1c98dec74facc0461653535021827dfc22119b
Steven Gao <stevenruidigao@gmail.com>
2025-01-13 17:25:47 -0500

    update option shim

--- utils/optionShim.js
+++ utils/optionShim.js
@@ -1,3 +1,6 @@
+// TODO: replace option with something supporting ESM.
 const option = require('option');
 
 export const none = option.none;
+export const some = option.some;
+export const fromNullable = option.fromNullable;
