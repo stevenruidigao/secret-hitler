ff8c6145696c68c3312998a57055f889ec9bb4d5
Steven Gao <stevenruidigao@gmail.com>
2025-01-13 11:22:31 -0500

    update import

--- src/frontend-scripts/components/section-main/GameChatItem.jsx
+++ src/frontend-scripts/components/section-main/GameChatItem.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { getNumberWithOrdinal, PLAYERCOLORS } from '../../constants';
+import { getNumberWithOrdinal, PLAYERCOLORS } from '../../constants.mjs';
 import { processEmotes } from '../../emotes';
 
 const GameChatItem = ({ chat, playerListPlayer, seatedUserNames, gameSettings, allEmotes, gameInfo }) => {
