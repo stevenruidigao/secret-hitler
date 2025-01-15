fab59b8dee4ea104445779cfd855084edb2acc10
Steven Gao <stevenruidigao@gmail.com>
2025-01-15 00:39:47 -0500

    fix brackets

--- routes/socket/game/end-game.mjs
+++ routes/socket/game/end-game.mjs
@@ -472,7 +472,7 @@ export const completeGame = (game, winningTeamName) => {
 									io.sockets.sockets.get(socketId).handshake.session.passport.user === player.username
 							);
 
-							io.sockets.sockets[playerSocketId].emit('gameSettings', player.gameSettings);
+							io.sockets.sockets.get(playerSocketId).emit('gameSettings', player.gameSettings);
 						}
 					} else {
 						if (isRainbow) {
