81a990cbd2f615ec892c26c98eb6e0952057fd1c
Steven Gao <stevenruidigao@gmail.com>
2025-01-14 23:51:40 -0500

    fix map

--- routes/socket/routes.mjs
+++ routes/socket/routes.mjs
@@ -541,7 +541,7 @@ export const socketRoutes = () => {
 			socket.on('leaveGame', data => {
 				const game = findGame(data);
 
-				if (game && game.general && io.sockets.adapter.rooms[game.general.uid] && socket) {
+				if (game && game.general && io.sockets.adapter.rooms.get(game.general.uid) && socket) {
 					socket.leave(game.general.uid);
 				}
 
